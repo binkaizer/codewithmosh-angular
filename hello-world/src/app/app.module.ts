94ba9ae3406a9a61626884024cda1dd059ad1186
TamPham <binemperor@gmail.com>
2019-01-17 14:44:41 +0700

    data binding

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
@@ -6,11 +7,16 @@ import { AppComponent } from "./app.component";
 import { CoursesComponent } from "./courses.component";
 import { CourseComponent } from "./course/course.component";
 import { CoursesService } from "./courses.service";
-import { AuthorsComponent } from './authors/authors.component';
+import { AuthorsComponent } from "./authors/authors.component";
 
 @NgModule({
-  declarations: [AppComponent, CoursesComponent, CourseComponent, AuthorsComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    CoursesComponent,
+    CourseComponent,
+    AuthorsComponent
+  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [CoursesService],
   bootstrap: [AppComponent]
 })
--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -8,14 +8,56 @@ import { CoursesService } from "./courses.service";
     <ul>
       <li *ngFor="let course of courses">{{ course }}</li>
     </ul>
+    <button class="btn btn-primary" [class.active]="isActive">Save</button>
+    <button [style.backgroundColor]="isActive ? 'blue' : 'white'">
+      Style Binding
+    </button>
+    <div (click)="onDivSave()">
+      <button (click)="onSave($event)">Event Binding</button>
+    </div>
+    <div><input (keyup.enter)="onKeyUp($event)" /></div>
+    <div><input #email2 (keyup.enter)="onKeyUpReference(email2.value)" /></div>
+    <div>
+      <input
+        [value]="email"
+        (keyup.enter)="email = $event.target.value; onKeyUp2WayBinding()"
+      />
+    </div>
+    <div>
+      <input [(ngModel)]="email" (keyup.enter)="onKeyUp2WayBinding()" />
+    </div>
   `
 })
 export class CoursesComponent {
   title = "List of courses";
   courses;
+  isActive = false;
+  email = "me@example.com";
 
   constructor(service: CoursesService) {
     // let service = new CoursesService();
     this.courses = service.getCourses();
+  }
+
+  onSave($event) {
+    event.stopPropagation();
+
+    console.log("Button was clicked", $event);
+  }
+
+  onDivSave() {
+    console.log("Div was clicked");
+  }
+
+  onKeyUp($event) {
+    console.log($event.target.value);
+  }
+
+  onKeyUpReference(email2) {
+    console.log(email2);
+  }
+
+  onKeyUp2WayBinding() {
+    console.log(this.email);
   }
 }
